8d1de3d38f4b6927c06253b8ab5b60d7139d3ed0
Stosh09 <stoshochieng@gmail.com>
2024-04-25 13:02:51 +0300

    Complete Rotate 2D Matrix challenge

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,29 +1,15 @@
 #!/usr/bin/python3
-"""2D matrix rotation module.
-"""
+""" Module that implements rotate_2d_matrix """
 
 
 def rotate_2d_matrix(matrix):
-    """Rotates an m by n 2D matrix in place.
-    """
-    if type(matrix) != list:
-        return
-    if len(matrix) <= 0:
-        return
-    if not all(map(lambda x: type(x) == list, matrix)):
-        return
-    rows = len(matrix)
-    cols = len(matrix[0])
-    if not all(map(lambda x: len(x) == cols, matrix)):
-        return
-    c, r = 0, rows - 1
-    for i in range(cols * rows):
-        if i % rows == 0:
-            matrix.append([])
-        if r == -1:
-            r = rows - 1
-            c += 1
-        matrix[-1].append(matrix[r][c])
-        if c == cols - 1 and r >= -1:
-            matrix.pop(r)
-        r -= 1
+    """ Function that rotates a 2d matrix in place """
+    # x = list(range(len(matrix)))
+    # print(x)
+    mat_len = len(matrix)
+    for i in range(mat_len):
+        for j in range(i + 1, mat_len):
+            # print(f'i = {i}, j = {j}')
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for i in range(mat_len):
+        matrix[i].reverse()
